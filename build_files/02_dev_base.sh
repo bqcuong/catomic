0c737e2f85217684ff7e64100ddac5c8eac5fb58
bqcuong <bqcuong2212@gmail.com>
2026-04-12 21:11:03 +0200

    fix atuin

--- build_files/02_dev_base.sh
+++ build_files/02_dev_base.sh
@@ -7,9 +7,6 @@ copr_install_isolated "dejan/lazygit" lazygit
 
 # TODO: neovim will be pre-installed in Fedora 25
 dnf5 install -y \
-    zsh btop fd-find \
+    zsh btop fd-find atuin \
     neovim python3-neovim \
     golang
-
-# Atuin for managing shell history
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh -s -- --non-interactive
