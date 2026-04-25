bdec9d35de3651f744cc1e2c0293f574733a761f
bqcuong <bqcuong2212@gmail.com>
2026-04-25 22:54:52 +0200

    up

--- build_files/04_sunshine.sh
+++ build_files/04_sunshine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -oue pipefail
 
+source /ctx/utils/copr_helpers.sh
+
 copr_install_isolated "lizardbyte/stable" Sunshine
 
 setcap 'cap_sys_admin+p' $(readlink -f /usr/bin/sunshine)
