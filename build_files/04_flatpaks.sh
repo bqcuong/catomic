e15822fdc939a2d73b48e340c32b770f4a480705
bqcuong <bqcuong2212@gmail.com>
2026-07-27 22:09:01 +0200

    add gaming flatpaks

---
+++ build_files/04_flatpaks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eou pipefail
+
+FLATPAK_LIST=$(echo \
+app/com.valvesoftware.Steam/x86_64/stable \
+app/net.lutris.Lutris/x86_64/stable \
+app/org.DolphinEmu.dolphin-emu/x86_64/stable \
+app/io.github.radiolamp.mangojuice/x86_64/stable \
+| tr '\n' ' ')
+
+flatpak --system -y install --reinstall --or-update ${FLATPAK_LIST}
