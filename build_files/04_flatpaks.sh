70c0099a258b33741a7e7e39dbbed7db7a83edcb
bqcuong <bqcuong2212@gmail.com>
2026-07-30 19:13:50 +0200

    add cemu

--- build_files/04_flatpaks.sh
+++ build_files/04_flatpaks.sh
@@ -5,6 +5,7 @@ FLATPAK_LIST=$(echo \
 app/com.valvesoftware.Steam/x86_64/stable \
 app/net.lutris.Lutris/x86_64/stable \
 app/org.DolphinEmu.dolphin-emu/x86_64/stable \
+app/info.cemu.Cemu/x86_64/stable \
 app/io.github.radiolamp.mangojuice/x86_64/stable \
 | tr '\n' ' ')
 
